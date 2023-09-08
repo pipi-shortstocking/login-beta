c441b7a09964d95d1c9f0a680154ed07f465e546
pipi-shortstocking <jannie435@naver.com>
2023-09-08 15:19:11 +0900

    table 생성 중

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const { sequelize } = require("./models");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection succeeded!");
+      console.log(`http://localhost:${PORT}/user`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ index.sql
@@ -0,0 +1,10 @@
+SHOW databases;
+DROP database sesac;
+
+create database sesac character set utf8mb4 collate utf8mb4_unicode_ci;
+
+USE sesac;
+
+DESC user;
+
+select * from user;
